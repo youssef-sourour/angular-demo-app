2cc516871bf4aa6943e39a54865928fcf9f15369
Youssef <mr.sourour@gmail.com>
2019-01-06 15:12:00 +0100

    Add ComponentsModule to app.module.ts (root app)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { ComponentsModule } from './components/components.module';
 
+import { AppComponent } from './app.component';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule, 
+    ComponentsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
