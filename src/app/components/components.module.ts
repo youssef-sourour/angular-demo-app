8574fc7d5c7f05376e4f5e62ffe25ceeaf08c8ac
Youssef <mr.sourour@gmail.com>
2019-01-06 15:10:50 +0100

    Generate compoents module and import HeaderComponent and FooterComponent

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './includes/header/header.component';
+import { FooterComponent } from './includes/footer/footer.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [FooterComponent, HeaderComponent],
   imports: [
     CommonModule
   ]
